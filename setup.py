6683f90c6b48a15570e41516d3d88e983f20b24c
Ioana Crisan <c_icrisan@groupon.com>
2020-05-27 15:58:21 +0300

    First setup commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+PACKAGE_NAME = 'tracking'
+PACKAGE_VERSION = '0.1'
+INSTALL_REQUIRES = [
+    'pytest == 5.4.1',
+    'requests == 2.23.0'
+]
+
+setup(
+    name=PACKAGE_NAME,
+    version=PACKAGE_VERSION,
+    description='API testing for tracking packages between deposits',
+    packages=find_packages(),
+    python_requires='>=3.7.3',
+
+    entry_points={
+        'console_scripts': [
+            'tracking = tracking.scripts.main:main'
+        ]
+    }
+)
